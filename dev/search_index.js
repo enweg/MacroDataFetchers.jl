6377478197cc938600188cb4632701a9c148dd22
Documenter.jl <documenter@juliadocs.github.io>
2026-04-01 13:28:03 +0000

    build based on 065b629

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MacroDataFetchers","page":"Home","title":"MacroDataFetchers","text":"Documentation for MacroDataFetchers.\n\n","category":"section"}]
+}
